a4cbca7161dc1e424fff03dcc95bf44cc036a154
Monson Reji Verghese <monsonreji2002@gmail.com>
2024-05-27 14:06:15 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
   return (
     <div className='w-full py-8'>
       <Container>
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center gap-1'>
           {posts && posts.length 
           ? posts.map((post) => (
             <div key={post.$id} className=''>
